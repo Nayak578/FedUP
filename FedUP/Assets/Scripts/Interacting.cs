f7f4aa4abe99f119792953ea387272a7091ad7f2
Aadarsh Nayak <www.nayakaadarsh2005@gmail.com>
2025-06-17 20:31:56 +0530

    Adding the Interacting Script

--- FedUP/Assets/Scripts/Interacting.cs
+++ FedUP/Assets/Scripts/Interacting.cs
@@ -1,10 +1,13 @@
+using TMPro;
 using UnityEngine;
 
 public class Interacting : MonoBehaviour
 {
-    [SerializeField] int interactionLayer=7;
     [SerializeField] float range = 2.5f;
     [SerializeField]LayerMask layer;
+    [SerializeField] GameObject E;
+    [SerializeField] GameObject Esc;
+    private Interactable interactable;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,9 +20,18 @@ public class Interacting : MonoBehaviour
         RaycastHit hit;
         Vector3 fwd = transform.TransformDirection(Vector3.forward);
         if (Physics.Raycast(transform.position, fwd, out hit, range, layer)) {
+            E.SetActive(true);
+            Esc.SetActive(true);
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            if (Input.GetKeyDown(KeyCode.E)) {
+                E.SetActive(false);
+                hit.collider.TryGetComponent<Interactable>(out interactable);
+                if(interactable.isActiveAndEnabled)interactable.Interact();
+            }
 
         } else {
+            E.SetActive(false);
+            Esc.SetActive(false);
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * range, Color.red);
 
         }
